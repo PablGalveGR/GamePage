b8d34488000119cc00e8a7b74cef0c75a825adbd
Pablo Galve <107808655+PablGalveGR@users.noreply.github.com>
2025-06-25 22:33:45 +0200

    Score board filtered
    refractor the code for the filtering of the scoreboard

--- Front/src/app/services/score/score.service.ts
+++ Front/src/app/services/score/score.service.ts
@@ -15,17 +15,38 @@ export class ScoreService {
   }
   getScoresByGame(id:number) : Observable<Score[]>{
     let scores : Score[] = this.scores.filter( score => score.game == id);
-    if(scores.length >0){
-
+    let users : Set<number> = new Set();
+    for(let score of scores){//gets all the users whom played the game
+      users.add(score.username);
     }
-    return of(scores);
+    let scoresSorted = scores.sort((score1, score2) =>score2.score - score1.score);//sort the array by score
+    let scoreFiltered : Score[] = [];
+    for(let user of users){
+      scoreFiltered.push(scoresSorted.find(score => score.username == user)!);
+    }
+    scoreFiltered = scoreFiltered.sort((score1, score2) =>{///Sorts the array by the score
+      if(score1.score > score2.score){
+        return -1;
+      }
+      if( score1.score < score2.score){
+        return 1;
+      }
+      return 0;
+    });
+    return of(scoreFiltered);
 
   }
-  /*getScoresByUser(id:number) : Observable<Score[]>{
-    let scores : Score[] = this.scores.filter( score => score.user == id);
-    if(scores.length >0){
-
-    }
+  getScoresByUser(id:number) : Observable<Score[]>{
+    let scores : Score[] = this.scores.filter( score => score.username == id);
+    let scoresSorted = scores.sort((score1, score2) =>{///Sorts the array by the score
+      if(score1.score > score2.score){
+        return 1;
+      }
+      if( score1.score < score2.score){
+        return -1;
+      }
+      return 0;
+    })
     return of(scores);
-  }*/
+  }
 }
