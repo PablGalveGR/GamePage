dc914715b2ab918f2ab04ada82647bc577049e9c
Pablo Galve <107808655+PablGalveGR@users.noreply.github.com>
2025-09-16 20:44:39 +0200

    some quick code removal

--- Front/src/app/components/game/game.component.ts
+++ Front/src/app/components/game/game.component.ts
@@ -35,38 +35,4 @@ export class GameComponent {
     this.gameService.getGame(this.id).subscribe(game => this.game = game)!;
     console.log("Game :" + this.game?.name + " retrieved");
   }
-  getUser(id: number): User {
-    return Array.from(this.users).find(user => user.id == id)!;
-  }
-  getName(id: number): String {
-    let name: String = "";
-    try {
-      name = Array.from(this.users).find(user => user.id == id)!.name;
-
-    } catch (error) {
-      name = "awA"
-    }
-    return name;
-  }
-  /*getScores(): void {
-    this.gameService.getScores(this.id).subscribe(async (scores) => {
-      let users: Set<number> = new Set();
-      for (let score of scores) {//gets all the users whom played the game
-        this.gameService.getUser(score.username).subscribe(user => this.users.add(user));
-        users.add(score.username);//User id
-      }
-      let scoresSorted = scores.sort((score1, score2) => score2.score - score1.score);//sort the array by score
-      let scoreFiltered: Score[] = [];
-
-      for (let user of users) {//Take only the highest score from each user
-        scoreFiltered.push(scoresSorted.find(score => score.username == user)!);
-      }
-      scoreFiltered = scoreFiltered.sort((score1, score2) => score2.score - score1.score);///Sorts the array by the score
-      console.log("Scores for the game:" + this.game?.name + " retrieved");
-      this.scores = of(scoreFiltered);
-    });
-  }*/
-  checkIfSession(): boolean {
-    return sessionStorage.length > 0;
-  }
 }
