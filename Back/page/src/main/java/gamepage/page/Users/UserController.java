1a84a1fcfbf5bb0640a3098f4e3815be1b46e613
Pablo Galve <107808655+PablGalveGR@users.noreply.github.com>
2025-10-01 17:22:49 +0200

    Hashing passwords and angular update

--- Back/page/src/main/java/gamepage/page/Users/UserController.java
+++ Back/page/src/main/java/gamepage/page/Users/UserController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 import jakarta.validation.Valid;
+import gamepage.page.Hashing.*;
 
 
 @CrossOrigin(origins = {"http://127.0.0.1:4200", "http://localhost:4200"})
@@ -60,19 +61,22 @@ public class UserController {// Never return the password to the Client
     return opUser.get().getId();
   }
   
-
   // Create
   @ResponseStatus(HttpStatus.CREATED)
   @PostMapping("")
-  void createUser(@Valid @RequestBody User user) {
+  void createUser(@Valid @RequestBody User user) throws Exception {
+    Hash newHash = new Hash();
+    user.setPasswd(newHash.getHash(user.getPasswd()));
     userRepository.createUser(user);
   }
-
+  //Update and delete are not properlys secured yet
   // Update
-  @ResponseStatus(HttpStatus.ACCEPTED)
+  /*@ResponseStatus(HttpStatus.ACCEPTED)
   @PutMapping("update/{id}")
-  void updateUser(@Valid @RequestBody User user, @PathVariable int id) {
+  void updateUser(@Valid @RequestBody User user, @PathVariable int id) throws Exception {
     // log.info(user.toString());
+    Hash newHash = new Hash();
+    user.setPasswd(newHash.getHash(user.getPasswd()));
     userRepository.updateUser(user, id);
   }
 
@@ -81,5 +85,5 @@ public class UserController {// Never return the password to the Client
   @DeleteMapping("delete/{id}")
   void deleteUser(@Valid @PathVariable int id) {
     userRepository.deleteUser(id);
-  }
+  }*/
 }
--- Front/src/app/components/signup-login/signup-login.component.ts
+++ Front/src/app/components/signup-login/signup-login.component.ts
@@ -5,6 +5,7 @@ import { LoginService } from '../../services/logIn/login.service';
 import { SignUpService } from '../../services/signup/sign-up.service';
 import { NgIf } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import * as CryptoJS from 'crypto-js';
 
 @Component({
   selector: 'app-signup-login',
@@ -32,14 +33,14 @@ export class SignupLoginComponent {
   };
   showFormLog() {
     this.loginForm = !this.loginForm;
-    if(this.loginForm){
+    if (this.loginForm) {
       this.signUpForm = false;
     }
     this.request.passwd = "";
   }
   showFormSign() {
     this.signUpForm = !this.signUpForm;
-    if(this.signUpForm){
+    if (this.signUpForm) {
       this.loginForm = false;
     }
     this.request.passwd = "";
@@ -50,7 +51,8 @@ export class SignupLoginComponent {
   createUser(): string {
     let error: string = "";
     if (this.checkForms(this.user)) {
-      this.signUpService.createUser(this.user)
+      this.user.passwd = CryptoJS.SHA256(this.user.passwd).toString();
+      this.signUpService.createUser(this.user);
       console.log("User sent " + this.user.name + '|' + this.user.passwd);
       this.user.name = "";
       this.user.passwd = "";
@@ -63,20 +65,23 @@ export class SignupLoginComponent {
 
   }
   login() {
-    this.loginService.login(this.request).subscribe(response => {
-      if (response) {
-        console.log(response.token);
-        if (response.user && response.user.name == this.request.name) {
-          //Save the session into the client server
-          sessionStorage.setItem(response.user.name, response.token);
-          console.log("Session initialized :" + response.user.name);
-          this.logged = true;
+    if (this.request.name.length > 0 && this.request.passwd.length > 0) {
+      this.request.passwd = this.request.passwd;//encrypt
+      this.loginService.login(this.request).subscribe(response => {
+        if (response) {
+          console.log(response.token);
+          if (response.user && response.user.name == this.request.name) {
+            //Save the session into the client server
+            sessionStorage.setItem(response.user.name, response.token);
+            console.log("Session initialized :" + response.user.name);
+            this.logged = true;
+          }
         }
-      }
-      else {
-        console.log("No response");
-      }
-    });
+        else {
+          console.log("No response");
+        }
+      });
+    }
     console.log("User " + this.request.name + '|' + this.request.passwd);
     this.showFormLog();
   }
